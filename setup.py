398ff0c2a0bf25b9d1ebe6e20eaea769db8e703f
arturluis <arturluis94@gmail.com>
2022-05-25 11:53:14 -0300

    Updated setup.py to 2.2.11.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hypermapper",
-    version="2.2.10",
+    version="2.2.11",
     description="HyperMapper is a multi-objective black-box optimization tool based on Bayesian Optimization.",
     long_description=long_description,
     long_description_content_type="text/markdown",
